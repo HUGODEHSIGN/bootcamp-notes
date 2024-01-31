3c0772d936c97a51272149e0b0367aa2d1407a6e
Hugo Hsi <hhsi@hugodehsign.com>
2024-01-31 02:52:12 -0500

    added an article card to go on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import ArticleCard from "@/components/ArticleCard";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="px-12"></main>;
+  return (
+    <main className="px-12">
+      <ArticleCard title="Title" description="Testing the description here" />
+    </main>
+  );
 }
