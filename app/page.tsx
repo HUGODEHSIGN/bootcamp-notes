a2bf01ea5d6089b0e36cb783b2a226d2aca0010b
Hugo Hsi <hhsi@hugodehsign.com>
2024-01-31 05:24:55 -0500

    fixed resonsibility on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="px-12">
+    <main className="mx-6 sm:mx-12 md:mx-24 lg:mx-48">
       <ArticleGrid />
+      <div className="w-24 h-24 bg-slate-100 sm:bg-slate-200 md:bg-slate-300 lg:bg-slate-300 xl:bg-slate-400 2xl:bg-slate-500"></div>
     </main>
   );
 }
