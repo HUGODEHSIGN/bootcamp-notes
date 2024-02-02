c9ebba7a339d6a138f3147fd523c8d28bf703df6
Hugo Hsi <hhsi@hugodehsign.com>
2024-02-02 03:43:39 -0500

    changed number of columns per mediaquery to prevent text overflow

--- components/ArticleGrid.tsx
+++ components/ArticleGrid.tsx
@@ -104,7 +104,7 @@ export default function ArticleGrid() {
     }
   }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
       {renderCategories()}
     </div>
   );
